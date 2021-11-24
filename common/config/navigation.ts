d5cbe0a757a66f58540ce6c9c29ac9a97df65af2
Xziy <x@42.pub>
2021-11-24 08:18:23 +0300

    init dubaicoin

--- common/config/navigation.ts
+++ common/config/navigation.ts
@@ -16,18 +16,18 @@ export const navigationLinks: NavigationLink[] = [
     name: 'NAV_GENERATEWALLET',
     to: '/generate'
   },
-  {
-    name: 'NAV_SWAP',
-    to: '/swap'
-  },
+  // {
+  //   name: 'NAV_SWAP',
+  //   to: '/swap'
+  // },
   {
     name: 'NAV_CONTRACTS',
     to: '/contracts'
   },
-  {
-    name: 'NAV_ENS',
-    to: '/ens'
-  },
+  // {
+  //   name: 'NAV_ENS',
+  //   to: '/ens'
+  // },
   {
     name: 'NAV_SIGN',
     to: '/sign-and-verify-message'
